3419c0fe90b2a8abb9c6de01edd6a01265969431
blsm <blsm@misena.edu.co>
2013-05-07 11:46:21 -0500

    Lazybox - tabla cortesias

--- app/controllers/cortesias_controller.rb
+++ app/controllers/cortesias_controller.rb
@@ -2,76 +2,37 @@ class CortesiasController < ApplicationController
 
   def index
     @cortesias = Cortesia.all
-
     respond_to do |format|
       format.html 
       format.json { render json: @cortesias }
     end
   end
 
-
   def show
-    @cortesia = Cortesia.find(params[:id])
-
-    respond_to do |format|
-      format.html 
-      format.json { render json: @cortesia }
-    end
+      @cortesia = Cortesia.find(params[:id])
   end
-
 
   def new
-    @cortesia = Cortesia.new
-
-    respond_to do |format|
-      format.html 
-      format.json { render json: @cortesia }
-    end
+      @cortesia = Cortesia.new
   end
-
 
   def edit
-    @cortesia = Cortesia.find(params[:id])
+      @cortesia = Cortesia.find(params[:id])
   end
-
 
   def create
-    @cortesia = Cortesia.new(params[:cortesia])
-
-    respond_to do |format|
-      if @cortesia.save
-        format.html { redirect_to @cortesia, notice: 'Cortesia was successfully created.' }
-        format.json { render json: @cortesia, status: :created, location: @cortesia }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @cortesia.errors, status: :unprocessable_entity }
-      end
-    end
+      @cortesia = Cortesia.new(params[:cortesia])
+      render :action => :new unless @cortesia.save
   end
-
 
   def update
-    @cortesia = Cortesia.find(params[:id])
-
-    respond_to do |format|
-      if @cortesia.update_attributes(params[:cortesia])
-        format.html { redirect_to @cortesia, notice: 'Cortesia was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @cortesia.errors, status: :unprocessable_entity }
-      end
-    end
+      @cortesia = Cortesia.find(params[:id])
+      render :action => :edit unless @cortesia.update_attributes(params[:cortesia])
   end
-
 
   def destroy
-    @cortesia = Cortesia.find(params[:id])
-    @cortesia.destroy
-
-    respond_to do |format|
-      format.html { redirect_to cortesias_url }
-      format.json { head :no_content }
-    end
+      @cortesia = Cortesia.find(params[:id])
+      @cortesia.destroy
   end
+  
 end
