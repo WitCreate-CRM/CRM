f639c365936a3605b13c1d59083987db29de2f29
blsm <blsm@misena.edu.co>
2013-05-10 11:10:39 -0500

    will_paginate, busqueda de registros, ordenamiento de columnas - tabla alimentos

--- app/controllers/alimentos_controller.rb
+++ app/controllers/alimentos_controller.rb
@@ -1,7 +1,13 @@
 class AlimentosController < ApplicationController
 
+  helper_method :sort_column, :sort_direction
+
   def index
-    @alimentos = Alimento.all
+    if params[:limit] == nil or params[:limit] <= "0" then
+          params[:limit] = 10
+    end
+    
+    @alimentos = Alimento.order(sort_column + ' ' + sort_direction).search(params[:search]).page(params[:page]).per_page(params[:limit].to_i)
     respond_to do |format|
       format.html 
       format.json { render json: @alimentos }
@@ -33,6 +39,16 @@ class AlimentosController < ApplicationController
   def destroy
       @alimento = Alimento.find(params[:id])
       @alimento.destroy
+  end
+
+ private
+ 
+  def sort_column
+    Alimento.column_names.include?(params[:sort]) ? params[:sort] : "id"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"
   end
   
 end
--- app/models/alimento.rb
+++ app/models/alimento.rb
@@ -1,6 +1,10 @@
 class Alimento < ActiveRecord::Base
   attr_accessible :descripcion
 
+  def self.search(search) 
+ 		where('descripcion like ? ', "%#{search}%" ) 
+  end
+
   validates :descripcion, :presence => true, 
    :length => { :maximum => 300 }
 
