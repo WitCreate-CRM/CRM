fae08716e6c0944424ec1ba0b0198d56ff79ba02
blsm <blsm@misena.edu.co>
2013-05-10 11:41:04 -0500

    will_paginate, busqueda de registros, ordenamiento de columnas - tabla cortesias

--- app/controllers/cortesias_controller.rb
+++ app/controllers/cortesias_controller.rb
@@ -1,7 +1,13 @@
 class CortesiasController < ApplicationController
 
+  helper_method :sort_column, :sort_direction
+
   def index
-    @cortesias = Cortesia.all
+    if params[:limit] == nil or params[:limit] <= "0" then
+          params[:limit] = 10
+    end
+
+    @cortesias = Cortesia.order(sort_column + ' ' + sort_direction).search(params[:search]).page(params[:page]).per_page(params[:limit].to_i)
     respond_to do |format|
       format.html 
       format.json { render json: @cortesias }
@@ -35,4 +41,14 @@ class CortesiasController < ApplicationController
       @cortesia.destroy
   end
   
+ private
+ 
+  def sort_column
+    Cortesia.column_names.include?(params[:sort]) ? params[:sort] : "id"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"
+  end
+
 end
