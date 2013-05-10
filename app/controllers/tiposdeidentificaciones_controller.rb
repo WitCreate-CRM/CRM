aca18a4b4fe812a7123b2c6657f4a0e20e466588
blsm <blsm@misena.edu.co>
2013-05-10 09:20:18 -0500

    Lazybox - tabla tiposdeidentificaciones

--- app/controllers/tiposdeidentificaciones_controller.rb
+++ app/controllers/tiposdeidentificaciones_controller.rb
@@ -1,83 +1,38 @@
 class TiposdeidentificacionesController < ApplicationController
-  # GET /tiposdeidentificaciones
-  # GET /tiposdeidentificaciones.json
+  
   def index
     @tiposdeidentificaciones = Tiposdeidentificacion.all
-
     respond_to do |format|
-      format.html # index.html.erb
+      format.html
       format.json { render json: @tiposdeidentificaciones }
     end
   end
 
-  # GET /tiposdeidentificaciones/1
-  # GET /tiposdeidentificaciones/1.json
   def show
-    @tiposdeidentificacion = Tiposdeidentificacion.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @tiposdeidentificacion }
-    end
+      @tiposdeidentificacion = Tiposdeidentificacion.find(params[:id])
   end
 
-  # GET /tiposdeidentificaciones/new
-  # GET /tiposdeidentificaciones/new.json
   def new
-    @tiposdeidentificacion = Tiposdeidentificacion.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @tiposdeidentificacion }
-    end
+      @tiposdeidentificacion = Tiposdeidentificacion.new
   end
 
-  # GET /tiposdeidentificaciones/1/edit
   def edit
-    @tiposdeidentificacion = Tiposdeidentificacion.find(params[:id])
+      @tiposdeidentificacion = Tiposdeidentificacion.find(params[:id])
   end
 
-  # POST /tiposdeidentificaciones
-  # POST /tiposdeidentificaciones.json
   def create
-    @tiposdeidentificacion = Tiposdeidentificacion.new(params[:tiposdeidentificacion])
-
-    respond_to do |format|
-      if @tiposdeidentificacion.save
-        format.html { redirect_to @tiposdeidentificacion, notice: 'Tiposdeidentificacion was successfully created.' }
-        format.json { render json: @tiposdeidentificacion, status: :created, location: @tiposdeidentificacion }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @tiposdeidentificacion.errors, status: :unprocessable_entity }
-      end
-    end
+      @tiposdeidentificacion = Tiposdeidentificacion.new(params[:tiposdeidentificacion])
+      render :action => :new unless @tiposdeidentificacion.save
   end
 
-  # PUT /tiposdeidentificaciones/1
-  # PUT /tiposdeidentificaciones/1.json
   def update
-    @tiposdeidentificacion = Tiposdeidentificacion.find(params[:id])
-
-    respond_to do |format|
-      if @tiposdeidentificacion.update_attributes(params[:tiposdeidentificacion])
-        format.html { redirect_to @tiposdeidentificacion, notice: 'Tiposdeidentificacion was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @tiposdeidentificacion.errors, status: :unprocessable_entity }
-      end
-    end
+      @tiposdeidentificacion = Tiposdeidentificacion.find(params[:id])
+      render :action => :edit unless @tiposdeidentificacion.update_attributes(params[:tiposdeidentificacion])
   end
 
-  # DELETE /tiposdeidentificaciones/1
-  # DELETE /tiposdeidentificaciones/1.json
   def destroy
-    @tiposdeidentificacion = Tiposdeidentificacion.find(params[:id])
-    @tiposdeidentificacion.destroy
-
-    respond_to do |format|
-      format.html { redirect_to tiposdeidentificaciones_url }
-      format.json { head :no_content }
-    end
+      @tiposdeidentificacion = Tiposdeidentificacion.find(params[:id])
+      @tiposdeidentificacion.destroy
   end
+  
 end
