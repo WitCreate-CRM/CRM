8750404b96db4ba04cf08d7b9569b37e2e9cd573
blsm <blsm@misena.edu.co>
2013-05-15 14:36:21 -0500

    will_paginate, busqueda de registros, ordenamiento de columnas - tabla preferencias

--- app/controllers/preferencias_controller.rb
+++ app/controllers/preferencias_controller.rb
@@ -1,8 +1,13 @@
 class PreferenciasController < ApplicationController
-  
+
+  helper_method :sort_column, :sort_direction  
 
   def index
-    @preferencias = Preferencia.all
+    if params[:limit] == nil or params[:limit] <= "0" then
+          params[:limit] = 10
+    end
+
+    @preferencias = Preferencia.order(sort_column + ' ' + sort_direction).search(params[:search]).page(params[:page]).per_page(params[:limit].to_i)
     respond_to do |format|
       format.html 
       format.json { render json: @preferencias }
@@ -36,4 +41,14 @@ class PreferenciasController < ApplicationController
       @preferencia.destroy
   end
   
+ private
+ 
+  def sort_column
+    Preferencia.column_names.include?(params[:sort]) ? params[:sort] : "id"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"
+  end
+
 end
--- app/models/preferencia.rb
+++ app/models/preferencia.rb
@@ -3,7 +3,11 @@ class Preferencia < ActiveRecord::Base
   belongs_to :huesped
   attr_accessible :numero_almohada_en_cama, :huesped_id, :tipo_habitacion_id
 
+  def self.search(search) 
+ 		where('numero_almohada_en_cama != ? ', "0" ) 
+  end
+
   validates :numero_almohada_en_cama, :presence => true, 
-   :length => { :maximum => 1 }
+   :length => { :maximum => 2 }
 
 end
