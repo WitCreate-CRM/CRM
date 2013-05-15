e731cef697303db6c22236f282ac0f5e34d160b2
blsm <blsm@misena.edu.co>
2013-05-15 14:50:36 -0500

    will_paginate, busqueda de registros, ordenamiento de columnas - tabla sugerenciasyqueja

--- app/controllers/sugerenciasyquejas_controller.rb
+++ app/controllers/sugerenciasyquejas_controller.rb
@@ -1,7 +1,13 @@
 class SugerenciasyquejasController < ApplicationController
-  
+ 
+  helper_method :sort_column, :sort_direction
+
 def index
-    @sugerenciasyquejas = Sugerenciasyqueja.all
+  if params[:limit] == nil or params[:limit] <= "0" then
+        params[:limit] = 10
+  end
+
+    @sugerenciasyquejas = Sugerenciasyqueja.order(sort_column + ' ' + sort_direction).search(params[:search]).page(params[:page]).per_page(params[:limit].to_i)
     respond_to do |format|
       format.html
       format.json { render json: @sugerenciasyquejas }
@@ -35,4 +41,14 @@ def index
       @sugerenciasyqueja.destroy
   end
   
+ private
+ 
+  def sort_column
+    Sugerenciasyqueja.column_names.include?(params[:sort]) ? params[:sort] : "id"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"
+  end
+
 end
--- app/models/sugerenciasyqueja.rb
+++ app/models/sugerenciasyqueja.rb
@@ -2,6 +2,10 @@ class Sugerenciasyqueja < ActiveRecord::Base
   belongs_to :huesped
   attr_accessible :observaciones, :problemas_en_la_estadia, :sugerencias, :huesped_id
 
+  def self.search(search) 
+ 		where('observaciones like ? OR problemas_en_la_estadia like ? OR sugerencias like ? ', "%#{search}%", "%#{search}%", "%#{search}%" ) 
+  end
+
   validates :observaciones, :presence => true,
    :length => { :maximum => 2500 }
 
