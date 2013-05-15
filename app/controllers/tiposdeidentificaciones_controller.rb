8097b602c4aae29f9bc32ba716a50b8693ad94e9
blsm <blsm@misena.edu.co>
2013-05-15 15:13:13 -0500

    will_paginate, busqueda de registros, ordenamiento de columnas - tabla tiposdeidenificaciones

--- app/controllers/tiposdeidentificaciones_controller.rb
+++ app/controllers/tiposdeidentificaciones_controller.rb
@@ -1,7 +1,13 @@
 class TiposdeidentificacionesController < ApplicationController
   
+  helper_method :sort_column, :sort_direction
+
   def index
-    @tiposdeidentificaciones = Tiposdeidentificacion.all
+    if params[:limit] == nil or params[:limit] <= "0" then
+          params[:limit] = 10
+    end 
+
+    @tiposdeidentificaciones = Tiposdeidentificacion.order(sort_column + ' ' + sort_direction).search(params[:search]).page(params[:page]).per_page(params[:limit].to_i)
     respond_to do |format|
       format.html
       format.json { render json: @tiposdeidentificaciones }
@@ -35,4 +41,14 @@ class TiposdeidentificacionesController < ApplicationController
       @tiposdeidentificacion.destroy
   end
   
+ private
+ 
+  def sort_column
+    Tiposdeidentificacion.column_names.include?(params[:sort]) ? params[:sort] : "id"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"
+  end
+
 end
--- app/models/tiposdeidentificacion.rb
+++ app/models/tiposdeidentificacion.rb
@@ -1,6 +1,10 @@
 class Tiposdeidentificacion < ActiveRecord::Base
   attr_accessible :descripcion, :sigla
 
+  def self.search(search) 
+ 		where('descripcion like ? OR sigla like ? ', "%#{search}%", "%#{search}%" ) 
+  end
+
   validates :descripcion, :presence => true,
    :length => { :minimum => 6, :maximum => 20 }
 
