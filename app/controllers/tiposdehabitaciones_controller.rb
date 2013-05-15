e8d3669152dc16d020b271332ff7770a01f060d6
blsm <blsm@misena.edu.co>
2013-05-15 15:02:01 -0500

    will_paginate, busqueda de registros, ordenamiento de columnas - tabla tiposdehabitaciones

--- app/controllers/tiposdehabitaciones_controller.rb
+++ app/controllers/tiposdehabitaciones_controller.rb
@@ -1,7 +1,13 @@
 class TiposdehabitacionesController < ApplicationController
   
+  helper_method :sort_column, :sort_direction
+
   def index
-    @tiposdehabitaciones = Tiposdehabitacion.all
+    if params[:limit] == nil or params[:limit] <= "0" then
+          params[:limit] = 10
+    end
+
+    @tiposdehabitaciones = Tiposdehabitacion.order(sort_column + ' ' + sort_direction).search(params[:search]).page(params[:page]).per_page(params[:limit].to_i)
     respond_to do |format|
       format.html
       format.json { render json: @tiposdehabitaciones }
@@ -35,4 +41,13 @@ class TiposdehabitacionesController < ApplicationController
       @tiposdehabitacion.destroy
   end
   
+ 
+  def sort_column
+    Tiposdehabitacion.column_names.include?(params[:sort]) ? params[:sort] : "id"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"
+  end
+
 end
--- app/models/tiposdehabitacion.rb
+++ app/models/tiposdehabitacion.rb
@@ -1,6 +1,10 @@
 class Tiposdehabitacion < ActiveRecord::Base
   attr_accessible :descripcion, :habitacion_tina, :piso
 
+  def self.search(search) 
+ 		where('descripcion like ? ', "%#{search}%" ) 
+  end
+
   validates :descripcion, :presence => true,
    :length => { :maximum => 30 }
 
