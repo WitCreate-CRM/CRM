b7eb64b2d6073fb09de0efa11c9dae59d92f2285
Alejandro Henao <ahenao83@misena.edu.co>
2013-05-16 10:34:47 -0500

    will paginate y busqueda de casos de uso de modelo empresas

--- app/controllers/empresas_controller.rb
+++ app/controllers/empresas_controller.rb
@@ -1,7 +1,11 @@
 class EmpresasController < ApplicationController
 
   def index
-    @empresas= Empresa.all
+    if params[:limit] == nil or params[:limit] <= "0" then
+          params[:limit] = 10
+    end
+
+    @empresas = Empresa.order(sort_column + ' ' + sort_direction).search(params[:search]).page(params[:page]).per_page(params[:limit].to_i)
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @empresas}
@@ -34,5 +38,14 @@ class EmpresasController < ApplicationController
       @empresa  = Empresa.find(params[:id])
       @empresa .destroy
   end
-  
+  private
+ 
+  def sort_column
+    Empresa.column_names.include?(params[:sort]) ? params[:sort] : "id"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"
+  end
+
 end
