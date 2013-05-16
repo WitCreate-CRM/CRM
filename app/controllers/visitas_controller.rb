378f9cb40330a0c1190f9a58b9a54544d2d4be8e
Alejandro Henao <ahenao83@misena.edu.co>
2013-05-16 09:44:49 -0500

    will paginate y busqueda de modelo visitas

--- app/controllers/visitas_controller.rb
+++ app/controllers/visitas_controller.rb
@@ -1,12 +1,16 @@
 class VisitasController < ApplicationController
 
   def index
-    @visitas= Visita.all
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @visitas }
+    if params[:limit] == nil or params[:limit] <= "0" then
+          params[:limit] = 10
     end
-  end
+
+    @visitas = Visita.order(sort_column + ' ' + sort_direction).search(params[:search]).page(params[:page]).per_page(params[:limit].to_i)
+    respond_to do |format| 
+    format.html # index.html.erb 
+    format.xml { render :xml => @visitas } 
+   end 
+     end
 
   def show
       @visita = Visita.find(params[:id])
@@ -33,6 +37,16 @@ class VisitasController < ApplicationController
   def destroy
       @visita  = Visita.find(params[:id])
       @visita .destroy
+  end
+
+  private
+ 
+  def sort_column
+    Alimento.column_names.include?(params[:sort]) ? params[:sort] : "id"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"
   end
   
 end
