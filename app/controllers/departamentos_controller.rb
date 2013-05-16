2e9766273eef5e53161976d5ac385706a884af54
Alejandro Henao <ahenao83@misena.edu.co>
2013-05-16 11:04:07 -0500

    will paginate y busqueda de casos de uso de modelo departamentos

--- app/controllers/departamentos_controller.rb
+++ app/controllers/departamentos_controller.rb
@@ -1,7 +1,12 @@
 class DepartamentosController < ApplicationController
 
   def index
-    @departamentos= Departamento.all
+
+    if params[:limit] == nil or params[:limit] <= "0" then
+          params[:limit] = 10
+    end
+
+    @departamentos = Departamento.order(sort_column + ' ' + sort_direction).search(params[:search]).page(params[:page]).per_page(params[:limit].to_i)
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @departamentos }
@@ -33,6 +38,15 @@ class DepartamentosController < ApplicationController
   def destroy
       @departamento  = Departamento.find(params[:id])
       @departamento .destroy
+  end
+  private
+ 
+  def sort_column
+    Departamento.column_names.include?(params[:sort]) ? params[:sort] : "id"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"
   end
   
 end
--- app/models/departamento.rb
+++ app/models/departamento.rb
@@ -1,3 +1,8 @@
 class Departamento < ActiveRecord::Base
   attr_accessible :nombre
+  
+ def self.search(search) 
+  where('nombre like ?', "%#{search}%") 
+ end
+
 end
