d653cc7818bdf651fe860577ccc17306857b5679
Alejandro Henao <ahenao83@misena.edu.co>
2013-05-16 11:42:25 -0500

    will paginate y busqueda de casos de uso de modelo contactos

--- app/controllers/contactos_controller.rb
+++ app/controllers/contactos_controller.rb
@@ -1,7 +1,12 @@
 class ContactosController < ApplicationController
 
   def index
-    @contactos= Contacto.all
+    
+    if params[:limit] == nil or params[:limit] <= "0" then
+          params[:limit] = 10
+    end
+
+    @contactos = Contacto.order(sort_column + ' ' + sort_direction).search(params[:search]).page(params[:page]).per_page(params[:limit].to_i)
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @contactos }
@@ -35,4 +40,13 @@ class ContactosController < ApplicationController
       @contacto .destroy
   end
   
+  private
+ 
+  def sort_column
+    Contacto.column_names.include?(params[:sort]) ? params[:sort] : "id"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"
+  end
 end
--- app/models/contacto.rb
+++ app/models/contacto.rb
@@ -1,4 +1,8 @@
 class Contacto < ActiveRecord::Base
   belongs_to :empresa
   attr_accessible :celular, :email, :fecha_nacimiento, :nombre, :empresa_id
+
+  def self.search(search) 
+  where('nombre like ?', "%#{search}%") 
+ end
 end
