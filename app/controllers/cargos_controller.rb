d0c48f817e19425b5f6ab38bf39712cfe2f0802f
Alejandro Henao <ahenao83@misena.edu.co>
2013-05-21 11:50:04 -0500

    ordenamiento departamentos,cargos,contactos,obsequios,empleados

--- app/controllers/cargos_controller.rb
+++ app/controllers/cargos_controller.rb
@@ -1,4 +1,6 @@
 class CargosController < ApplicationController
+  
+  helper_method :sort_column, :sort_direction
 
   def index
 
--- app/controllers/obsequios_controller.rb
+++ app/controllers/obsequios_controller.rb
@@ -1,5 +1,7 @@
 class ObsequiosController < ApplicationController
 
+    helper_method :sort_column, :sort_direction
+
   def index
     
     if params[:limit] == nil or params[:limit] <= "0" then
